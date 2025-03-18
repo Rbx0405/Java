8eedaac1f66e7dc54b3be6bf5fd34ef3771b604f
Rishit Butola <rishitbutola10@gmail.com>
2025-03-18 12:47:07 +0530

    constructor file

---
+++ Constructor/construct.java
@@ -0,0 +1,9 @@
+class Construct {
+    Construct() {
+        System.out.println("Constructor is called!");
+    }
+
+    public static void main(String[] args) {
+        Construct obj = new Construct();
+    }
+}
