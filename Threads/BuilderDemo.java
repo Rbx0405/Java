2afbc284feb0aa8b43c9d82706bb3d8a258c6d4a
Rishit Butola <rishitbutola10@gmail.com>
2025-05-26 08:50:04 +0530

    StrBUilder

---
+++ Threads/BuilderDemo.java
@@ -0,0 +1,7 @@
+public class BuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+        sb.append(" Rocks");
+        System.out.println(sb); // Output: Java Rocks
+    }
+}
