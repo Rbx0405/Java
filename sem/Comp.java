8ce883bf12175b0020ee94b978a924fccff18c64
Rishit Butola <rishitbutola10@gmail.com>
2025-06-22 14:27:32 +0530

    comparison op

---
+++ sem/Comp.java
@@ -0,0 +1,8 @@
+public class Comp {
+    public static void main(String[] args) {
+        int x = 5, y = 10;
+        System.out.println(x == y); // false
+        System.out.println(x != y); // true
+        System.out.println(x < y);  // true
+    }
+}
